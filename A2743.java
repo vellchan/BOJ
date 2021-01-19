fa26870619907f8dcd1b695e4a967a380050dd36
JongChan0831 <72534445+dlwhdcks34@users.noreply.github.com>
2021-01-19 22:36:35 +0900

    [BOJ] 2743 - 단어 길이 재기

---
+++ A2743.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class A2743 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char[] cArr = s.toCharArray();
+		System.out.println(cArr.length);
+	}
+
+}
